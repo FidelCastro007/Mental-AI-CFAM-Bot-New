ed60e5cf206be79f9cbdb1a772a293fc2b0ab63a
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-11-04 14:59:38 +0530

    Update MentalAI.js

--- Public/MentalAI.js
+++ Public/MentalAI.js
@@ -103,7 +103,7 @@ async function sendMessage() {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-              apiKey:process.env.API_KEY,
+              apiKey:"VbjlwpLV4td0t2kd6q8FiAOB1GO2",
                 messages: [{ role: 'user', content: userInput }],
             }),
         });
